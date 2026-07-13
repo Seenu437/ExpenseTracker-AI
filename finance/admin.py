db758d30dc93464927819c5cc0cf0bc11d349ed2
Seenivasan <2303031241224@paruluniversity.ac.in>
2026-07-13 19:27:21 +0530

    Delete finance directory

--- finance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Transaction, Budget, Insight
-
-# This tells Django to show these tables in your dashboard
-admin.site.register(Category)
-admin.site.register(Transaction)
-admin.site.register(Budget)
-admin.site.register(Insight)
